eee104e594afaefbd3f2ec8abcaa034f156b163a
jimleelcc <jimleelcc@163.com>
2019-01-29 20:40:35 +0800

    实现了标题到正文的跳转

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 
@@ -8,3 +8,8 @@ from .models import Blog
 def blog_page(request):
     blogs = Blog.objects
     return render(request, 'blog.html', {'blogs': blogs})
+
+
+def blog_text(request, blog_id):
+    blog = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'blog_text.html', {'blog': blog})
